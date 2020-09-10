163e181c90bf93aad403a748707360b20e9f6348
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-09-10 00:05:33 -0500

    add prop types and props to show the book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Book() {
+export default function Book(props) {
+  const { book } = props;
+  const { idBook, title, category } = book;
   return (
     <tr>
-      <td>1</td>
-      <td>Harry Potter</td>
-      <td>Fiction</td>
+      <td>{idBook}</td>
+      <td>{title}</td>
+      <td>{category}</td>
+      <td>
+        <button type="button" onClick="">Delete</button>
+      </td>
     </tr>
   );
 }
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    idBook: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
