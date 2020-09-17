c5a91ad05fd02003fd13e797241afcecedeb1b1c
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-09-17 12:39:45 -0500

    update reducer parameters names for filter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 import booksReducer from './books';
 import filterReducer from './filter';
 
-export default function rootReducer(initialState) {
+export default function rootReducer(initialState, filter) {
   return combineReducers({
     booksReducer: booksReducer(initialState),
-    filterReducer: filterReducer(initialState),
+    filterReducer: filterReducer(filter),
   });
 }
