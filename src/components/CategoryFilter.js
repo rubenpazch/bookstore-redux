3af844d124a1af3be0da7c572d85e4c07d3a0441
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-09-17 12:39:01 -0500

    add proptypes for handle filter change

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-function CategoryFilter() {
+function CategoryFilter({ handleFilterChange }) {
   return (
     <label htmlFor="book-title">
       Categorie
-      <select name="category" id="category">
+      <select name="category" id="category" onChange={e => handleFilterChange(e.target.value)}>
         <option>All</option>
         {categories.map(c => <option key={c} value={c}>{c}</option>)}
       </select>
     </label>
   );
 }
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
 
 export default CategoryFilter;
 
