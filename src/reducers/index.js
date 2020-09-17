a904a5ffc50fbebfef4e1c1f5b23445118284668
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-09-17 11:07:00 -0500

    add filter reducer to combine on root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books';
+import filterReducer from './filter';
 
 export default function rootReducer(initialState) {
   return combineReducers({
     booksReducer: booksReducer(initialState),
+    filterReducer: filterReducer(initialState),
   });
 }
