0c5411a25076e1045eaae1aac10389de622ff16c
ruben paz chuspe <rubenpazchuspe@outlook.com>
2020-09-22 10:52:51 -0500

    add style for title components

--- src/components/App.js
+++ src/components/App.js
@@ -10,32 +10,32 @@ function App() {
   return (
     <Container className="panel-main">
       <Row className="panel-bg">
-        <Col sm={7} className="red">
-          <Row className="chartreuse">
-            <Col sm={6} className="Bookstore-CMS chocolate">BookStore CMS</Col>
+        <Col sm={7} className="panel-title">
+          <Row className="panel-title-row">
+            <Col sm={6} className="Bookstore-CMS">BookStore CMS</Col>
             <Col sm={6}>
               <Row>
-                <Col sm={3} className="BOOKS  chocolate">Books</Col>
-                <Col sm={3} className="BOOKS  chocolate">Categories</Col>
+                <Col sm={3} className="BOOKS">Books</Col>
+                <Col sm={3} className="BOOKS">Categories</Col>
               </Row>
             </Col>
           </Row>
         </Col>
-        <Col sm={5} className="peru">
-          <Row>
+        <Col sm={5} className="panel-title-icon">
+          <Row className="panel-icon">
             <Col>
               <Image src="holder.js/171x180" roundedCircle />
             </Col>
           </Row>
         </Col>
       </Row>
-      <Row>
-        <Col sm={12}>
+      <Row className="listBookStore">
+        <Col sm={12} className="itemBook">
           <h4>Action</h4>
           <h1>The Hunger Games</h1>
           <h5>Sizenne Collins</h5>
         </Col>
-        <Col sm={12}>
+        <Col sm={12} className="itemBook">
           <h4>Action</h4>
           <h1>The Hunger Games</h1>
           <h5>Sizenne Collins</h5>
