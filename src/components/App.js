3e321bfd0b40e3cb09eddefd0761876c9ee1a53a
ruben paz chuspe <rubenpazchuspe@outlook.com>
2020-09-22 14:11:34 -0500

    adding style for title author and others

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import BooksForm from './BooksForm';
 import BooksList from './BooksList';
 
 function App() {
+  const url = '';
   return (
     <Container className="panel-main">
       <Row className="panel-bg">
@@ -23,17 +25,39 @@ function App() {
         </Col>
         <Col sm={5} className="panel-title-icon">
           <Row className="panel-icon">
-            <Col>
-              <Image src="holder.js/171x180" roundedCircle />
-            </Col>
+            <div className="rounderIcon">
+              <FontAwesomeIcon icon={faUser} />
+            </div>
           </Row>
         </Col>
       </Row>
       <Row className="listBookStore">
         <Col sm={12} className="itemBook">
-          <h4>Action</h4>
-          <h1>The Hunger Games</h1>
-          <h5>Sizenne Collins</h5>
+          <Row>
+            <Col>
+              <h4 className="category">Action</h4>
+              <h1 className="MovieTitle">The Hunger Games</h1>
+              <h5 className="Author">Sizenne Collins</h5>
+              <ul className="OptionLinks">
+                <li>
+                  <a href={url} className="Links">Comments</a>
+                </li>
+                <li>
+                  <a href={url} className="Links">Remove</a>
+                </li>
+                <li>
+                  <a href={url} className="Links">Edit</a>
+                </li>
+              </ul>
+            </Col>
+            <Col>
+              <span>Completed</span>
+            </Col>
+            <Col>
+              <p>Current Chapter</p>
+            </Col>
+          </Row>
+
         </Col>
         <Col sm={12} className="itemBook">
           <h4>Action</h4>
