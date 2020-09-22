39b9f7b0d5ff770e6fb409e40ff619adbdf11170
ruben paz chuspe <rubenpazchuspe@outlook.com>
2020-09-22 10:33:14 -0500

    add multiple layout component to the book store

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import BooksList from './BooksList';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import BooksForm from './BooksForm';
+import BooksList from './BooksList';
 
 function App() {
   return (
-    <div>
-      <BooksList />
-      <BooksForm />
-    </div>
+    <Container className="panel-main">
+      <Row className="panel-bg">
+        <Col sm={7} className="red">
+          <Row className="chartreuse">
+            <Col sm={6} className="Bookstore-CMS chocolate">BookStore CMS</Col>
+            <Col sm={6}>
+              <Row>
+                <Col sm={3} className="BOOKS  chocolate">Books</Col>
+                <Col sm={3} className="BOOKS  chocolate">Categories</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={5} className="peru">
+          <Row>
+            <Col>
+              <Image src="holder.js/171x180" roundedCircle />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <h4>Action</h4>
+          <h1>The Hunger Games</h1>
+          <h5>Sizenne Collins</h5>
+        </Col>
+        <Col sm={12}>
+          <h4>Action</h4>
+          <h1>The Hunger Games</h1>
+          <h5>Sizenne Collins</h5>
+        </Col>
+      </Row>
+    </Container>
+
   );
 }
 
