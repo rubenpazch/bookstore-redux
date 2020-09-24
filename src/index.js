eef2e0774dd555d68031aa4b411c8c25f69e587d
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-23 22:42:29 -0500

    add progress bar into the project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import { store } from './store/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './css/App.css';
+import './css/progressBar.css';
 
 ReactDOM.render(
   <Provider store={store}>
